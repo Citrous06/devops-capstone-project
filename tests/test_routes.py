73e32b705ea8a87237a96258854a43411cbd5596
Citrous06 <jewall009@yahoo.com>
2024-08-12 19:58:15 -0400

    fix: BASE_URL goof

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -128,7 +128,7 @@ class TestAccountService(TestCase):
     def test_list_accounts(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
-        response= self.client.get({BASE_URL})
+        response= self.client.get(BASE_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        data =response.get_json()
+        data= response.get_json()
         self.assertEqual(len(data), 5)
