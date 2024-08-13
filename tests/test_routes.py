fb66eac1a54525bdd18a06ff169949dad8fb2fbb
Citrous06 <jewall009@yahoo.com>
2024-08-13 01:16:57 -0400

    style: linting fix

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,7 +12,7 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
-from service import talisman  
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -24,7 +24,10 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
+
     """Account Service Tests"""
 
     @classmethod
@@ -130,15 +133,15 @@ class TestAccountService(TestCase):
     def test_list_accounts(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
-        response= self.client.get(BASE_URL)
+        response = self.client.get(BASE_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        data= response.get_json()
+        data = response.get_json()
         self.assertEqual(len(data), 5)
 
     def test_read_an_account(self):
         """It should Read a given Account"""
-        account= self._create_accounts(1)[0]
-        response = self.client.get(f"{BASE_URL}/{account.id}", content_type= "application/json")
+        account = self._create_accounts(1)[0]
+        response = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(data["name"], account.name)
@@ -165,7 +168,7 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         response = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    
+
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
@@ -183,7 +186,7 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-            
+
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
