04d773f326f89c5e52e7b6578bf79817ba9bd4c0
Citrous06 <jewall009@yahoo.com>
2024-08-12 21:26:47 -0400

    feat: delete accounts

--- service/routes.py
+++ service/routes.py
@@ -105,6 +105,14 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """Deletes an Account based on account_id"""
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
